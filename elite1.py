1e3f84ff5a4eba6c5ceb56f2d592fa9fcd5763ae
Hamayun musa <68311576+hamayunpro@users.noreply.github.com>
2021-10-30 11:48:44 +0500

    Update elite1.py

--- elite1.py
+++ elite1.py
@@ -58,8 +58,6 @@ def clear():
     if "linux" in sys.platform.lower():os.system("clear")
     elif "win" in sys.platform.lower():os.system("cls")
     else:os.system("clear")
-def banner():
-    print("\n%s    _________ __     \n%s   / ____/ (_) /____  ║ %sDesign By %sHAMAYUN %s& %sKHAN\n%s  / __/ / / / __/ _ \ ║ %sFB %s: %sFacebook.com/hamayun Khan\n%s / / / / / / /  / ║ %sFB %s: %sFacebook.com/HAmaYUN\n%s/_____/_/_/\__/\___/  ║ %sGithub %s: %sGithub.com/hamayun01\n"%(O,O,P,O,P,O,O,P,O,P,O,P,O,P,O,P,O,P))
 def menu_log():
     os.system('rm -rf token.txt')
     clear()
